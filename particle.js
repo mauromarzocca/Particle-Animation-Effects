c54a931143000917241973579d4a478a5f98d09f
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2022-10-12 11:03:16 +0200

    Add Style CSS

---
+++ particle.js
@@ -0,0 +1,34 @@
+let canvas = document.qwerySelector('canvas');
+let ctx = canvas.getContext('2d');
+let colors = ['pink', 'gray', 'cornflowerblue', 'red', 'orange', 'purple', 'yellow'];
+let mouse = {
+    x: window.innerWidth / 2,
+    y: windows.innerHeight / 2
+};
+
+let particles;
+let particleCount = 400;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Particle {
+    constructor() {
+        this.init()
+    }
+    init() {
+        this.position = {
+            x: mouse.x,
+            y: mouse.y
+        }
+        this.speed = 5;
+        this.velocity = {
+            x: Math.random() * this.speed,
+            y = Math.random() * this.speed
+        }
+        this.size = Math.random() * 5;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+
+        if(Math.random() < .5) {}
+    }
+}
